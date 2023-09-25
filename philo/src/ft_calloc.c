b241a7a8405c9ff5e3ff8c7590b5d0203af420b4
Vahdet Parlak <vparlak@k2m05s06.42kocaeli.com.tr>
2023-09-25 15:53:00 +0300

    ft_calloc.c added

---
+++ philo/src/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 15:08:55 by vparlak           #+#    #+#             */
+/*   Updated: 2023/09/25 15:30:22 by vparlak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ret;
+
+	ret = malloc(count * size);
+	if (ret)
+		memset(ret, '\0', (count * size));
+	return (ret);
+}
