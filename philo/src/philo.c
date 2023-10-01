168dba0346c7b2562a00707f8c64c185fbd9a800
Vahdet Parlak <vparlak@k2m08s04.42kocaeli.com.tr>
2023-10-01 17:43:56 +0300

    mutex_initilization

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,12 +6,13 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:42:05 by vparlak           #+#    #+#             */
-/*   Updated: 2023/09/25 19:45:03 by vparlak          ###   ########.fr       */
+/*   Updated: 2023/10/01 16:27:13 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 void	ft_init_vars(t_vars *vars, char **argv)
 {
@@ -20,24 +21,42 @@ void	ft_init_vars(t_vars *vars, char **argv)
 	vars->t_to_eat = ft_atoi(argv[2]);
 	vars->t_to_sleep = ft_atoi(argv[3]);
 	vars->eat_per_phil = ft_atoi(argv[4]);
+	vars->philo = (t_philo *)ft_calloc(vars->n_of_philo, sizeof(t_philo));
 }
+
+int	ft_mutex_init(t_vars *vars)
+{
+	int	i;
+
+	i = 0;
+	while (i < vars->n_of_philo)
+	{
+		if (pthread_mutex_init(&vars->philo[i].mutex.mutex_id , NULL))
+			return (1);
+		vars->philo[i].mutex.i_m = i;
+		i++;
+	}
+	return (0);
+}
+
+
 
 int	main(int argc, char *argv[])
 {
-	int		i;
 	t_vars	*vars;
- 
-	i = 1;
+
 	if (argc == 5 || argc == 6)
 	{
 		vars = &(t_vars){0};
 		ft_init_vars(vars, &argv[1]);
-		while (i < argc)
+		if (!vars->philo)
+			return (write(2, "Malloc Error!\n", 14), 1);
+		if (ft_mutex_init(vars))
 		{
-			printf("%d", ft_atoi(argv[i]));
-			i++;
+			free(vars->philo);
+			return(write(2, "Mutex Error!\n", 13), 1);
 		}
-
+		free(vars->philo);
 	}
 	else
 		write(2, "You must enter 4 or 5 arguments!\n", 33);
--- philo/src/philo.h
+++ philo/src/philo.h
@@ -6,7 +6,7 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:40:01 by vparlak           #+#    #+#             */
-/*   Updated: 2023/09/25 18:11:30 by vparlak          ###   ########.fr       */
+/*   Updated: 2023/10/01 16:26:31 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,32 @@
 # define PHILO_H
 
 # include <stddef.h>
+# include <pthread.h>
 # include <stdio.h> // silinecek
+
+typedef struct s_mutex
+{
+	pthread_mutex_t	mutex_id;
+	int				i_m;
+}	t_mutex;
+
+typedef struct s_philo
+{
+	pthread_t	thread;
+	t_mutex		mutex;
+}	t_philo;
 
 typedef struct s_vars
 {
-	int	n_of_philo;
-	int	t_to_die;
-	int	t_to_eat;
-	int	t_to_sleep;
-	int	eat_per_phil;
+	int		n_of_philo;
+	int		t_to_die;
+	int		t_to_eat;
+	int		t_to_sleep;
+	int		eat_per_phil;
+	t_philo	*philo;
+	int		i;
 }	t_vars;
+
 
 void	*ft_calloc(size_t count, size_t size);
 int		ft_atoi(const char *str);
